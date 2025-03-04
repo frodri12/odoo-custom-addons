dd0fd8f8ae87c9bd63582d4488d3f7b4ed6b7d95
frodri12 <fndorod@gmail.com>
2025-03-04 10:12:19 -0300

    Add default country on new partners

--- l10n_py/models/res_partner.py
+++ l10n_py/models/res_partner.py
@@ -8,6 +8,11 @@ import re
 
 _logger = logging.getLogger(__name__)
 
+ADDRESS_FIELDS = (
+    'street', 'l10n_py_house', 'street2', 
+    'zip', 'city', 'state_id', 'l10n_py_district_id', 
+    'l10n_py_city_id', 'country_id')
+
 class ResPartner(models.Model):
 
     _inherit = 'res.partner'
@@ -16,3 +21,16 @@ class ResPartner(models.Model):
     l10n_py_district_id = fields.Many2one("l10n_py_district")
     l10n_py_city_id = fields.Many2one("l10n_py_city")
 
+    @api.model
+    def default_get(self,fields_list):
+        res = super().default_get(fields_list)
+        res.update(
+            {'country_id':self.env['res.country'].search([('code', '=', 'PY')], limit=1).id}
+        )
+        res.update({'lang':self.env.lang})
+        return res
+
+    @api.onchange('country_id','l10n_py_city_id')
+    def _onChange_City(self):
+        if self.country_id.code == 'PY' and self.l10n_py_city_id.country_id.code == 'PY' :
+            self.write({'city': self.l10n_py_city_id.name,})
