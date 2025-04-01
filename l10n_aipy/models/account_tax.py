70481f48fbb9f8643f235f6a4650009749ca8e0c
frodri12 <fndorod@gmail.com>
2025-04-01 15:02:03 -0300

    Add client data on EDI

--- l10n_aipy/models/account_tax.py
+++ l10n_aipy/models/account_tax.py
@@ -9,4 +9,13 @@ class AccountTax(models.Model):
     l10n_aipy_tax_base = fields.Float(string='Base Imponible', 
           help='Base imponible para el cálculo del impuesto', default = 100.0)
 
+    l10n_aipy_tax_type = fields.Selection([
+        ('1', 'IVA'),
+        ('2', 'ISC'),
+        ('3', 'Renta'),
+        ('4', 'Ninguno'),
+        ('5', 'IVA - Renta'),
+    ], string='Tipo de impuesto', default='1',
+       help='Tipo de impuesto para el cálculo del mismo')
+
           
--- l10n_aipy_edi/models/account_move.py
+++ l10n_aipy_edi/models/account_move.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import logging
 import pytz
 import json
+import re
 
 _logger = logging.getLogger(__name__)
 
@@ -65,6 +66,7 @@ class AccountMove(models.Model):
         items = []
         isService = False
         isProduct = False
+        globalTaxTye = "0"
         for rec in self.invoice_line_ids:
             productId =rec.product_id
             uomId = productId.uom_id
@@ -95,6 +97,27 @@ class AccountMove(models.Model):
             item.update({"iva": ivaAmount}) #E734
             # E8.4 E750-E760 - Pendiente de implementar
             # E8.5 E770-E789 - Pendiente de implementar
+            #
+            taxType = rec.tax_ids.l10n_aipy_tax_type
+            if globalTaxTye == "0":
+                globalTaxTye = taxType
+            elif globalTaxTye == "5":
+                if taxType == "2":
+                    globalTaxTye = "2"
+            elif globalTaxTye == "4":
+                globalTaxTye = taxType
+            elif globalTaxTye == "3":
+                if taxType == "1" or taxType == "5":
+                    globalTaxTye = "5"
+                elif taxType == "2":
+                    globalTaxTye = "2"
+            elif globalTaxTye == "1":
+                if taxType == "3" or taxType == "5":
+                    globalTaxTye = "5"
+                elif taxType == "2":
+                    globalTaxTye = "2"
+
+            #
             if productId.type == 'combo':
                 isService = True
                 isProduct = True
@@ -112,19 +135,27 @@ class AccountMove(models.Model):
             tipoTransaccion = 2
         else:
             tipoTransaccion = 1
-        if self.l10n_latam_document_type_id.code == 'FA' and tipoTransaccion != 0:
+        if self.move_type == 'out_invoice' and tipoTransaccion != 0:
             self._setD("tipoTransaccion", tipoTransaccion) #D011
+        self._setD("tipoImpuesto", globalTaxTye) #D013
         return items
 
+    #############################
     def _compute_economic_activity( self):
         ecos = []
+        ecos_count = 0
         for rec in self.company_id.l10n_aipy_economic_activity_ids:
             eco = {}
-            eco.update({"codigo": rec.code})
-            eco.update({"descripcion": rec.name})
+            eco.update({"codigo": rec.code}) #D131
+            eco.update({"descripcion": rec.name}) #D132
             ecos.append(eco)
-        return ecos
+            ecos_count += 1
+        if ecos_count == 0:
+            raise UserError(_("Economic activity is required"))
+        else:
+            self._setP("actividadesEconomicas", ecos)
                 
+    #############################
     def _compute_estabecimientos( self):
         estabecimientos = []
         est = {}
@@ -135,19 +166,140 @@ class AccountMove(models.Model):
         if self.company_id.street2:
             est.update({"complementoDireccion1": self.company_id.street2}) #D109
         est.update({"departamento": self.company_id.state_id.code}) #D111
-        est.update({"ciudad": self.company_id.l10n_aipy_city_id.code}) #D115
         est.update({"distrito": self.company_id.l10n_aipy_district_id.code}) #D113
-        est.update({"telefono": self.company_id.phone}) #D117
-        est.update({"email": self.company_id.email}) #D118
+        est.update({"ciudad": self.company_id.l10n_aipy_city_id.code}) #D115
+        telefono = self.company_id.phone
+        if telefono:
+            if telefono.__len__() < 6 or telefono.__len__() > 15:
+                raise UserError(_("Phone number must be between 6 and 15 digits"))
+            else:
+                est.update({"telefono": telefono}) #D117
+        else:
+            raise UserError(_("Phone number is required"))
+        email = self.company_id.email
+        if email:
+            if email.find(",") > -1:
+                est.update({"email": email.splat(",")[0]}) #D118
+            else:
+                est.update({"email": email}) #D118
+        else:
+            raise UserError(_("Email is required"))
         estabecimientos.append(est)
         return estabecimientos
                 
     def _compute_regimeType( self):
         if self.company_id.l10n_aipy_regime_type_id:
             value = self.company_id.l10n_aipy_regime_type_id.code
-            if value > 0:
+            if value and value > 0:
                 self._setP("tipoRegimen", value)
                 
+    #############################
+    def _compute_timbrado( self):
+        """
+        Compute the timbrado number and date
+        """
+        auth_code = self.company_id.l10n_aipy_dnit_auth_code
+        auth_date = self.company_id.l10n_aipy_dnit_auth_date
+        if self.company_id.l10n_aipy_testing_mode:
+            auth_code = self.company_id.l10n_aipy_dnit_auth_code_test
+            auth_date = self.company_id.l10n_aipy_dnit_auth_date_test
+        if not auth_code or not auth_date:
+            raise UserError(_("Timbrado number and date are required"))
+        if auth_code.__len__() != 8 and auth_code.__len__() != 11:
+            raise UserError(_("Timbrado number must be 8 digits (12345678) or 11 digits (AA-12345678)"))
+        if auth_code.__len__() == 8:
+            self._setP("timbradoNumero", auth_code) #C004
+        elif auth_code.__len__() == 11:
+            if auth_code.split("-").__len__() != 2:
+                raise UserError(_("Timbrado number must be 8 digits (12345678) or 11 digits (AA-12345678)"))
+            self._setP("timbradoNumero", auth_code.split("-")[1]) #C004
+            self._setP("numeroSerie", auth_code.split("-")[0]) #C010
+        if auth_date:
+            self._setP("timbradoFecha", auth_date.strftime("%Y-%m-%d")) #C008
+        else:
+            raise UserError(_("Timbrado date is required"))
+
+    def _compute_digital_date( self):
+        """
+        Compute the digital date
+        """
+        now = datetime.now()
+        user = self.env['res.users'].browse([2])
+        tz = pytz.timezone(user.tz) or pytz.utc
+        user_tz = pytz.utc.localize(now).astimezone(tz)
+        return user_tz.strftime("%Y-%m-%dT%H:%M:%S") #A004
+
+    def _compute_sequence_number( self):
+        sequence_number = self.name
+        if re.findall( "^\\d{3}-\\d{3}-\\d{7}$", sequence_number.split(" ")[1]) == 0:
+            raise UserError(_("Sequence number format is invalid"))
+        number = sequence_number.split(" ")[1]
+        self._setD( "establecimiento", number.split("-")[0])
+        self._setD( "punto", number.split("-")[1]) #C006
+        self._setD( "numero", number.split("-")[2]) #C010
+
+    def _compute_client( self):
+        """
+        Compute the client data
+        """
+        cliente = {}
+        pais = self.partner_id.country_id.l10n_aipy_alpha_code
+        departamento = self.partner_id.state_id.code
+        distrito = self.partner_id.l10n_aipy_district_id.code
+        ciudad = self.partner_id.l10n_aipy_city_id.code
+        if self.partner_id.vat:
+            if self.partner_id.l10n_latam_identification_type_id.l10n_aipy_dnit_code == 0 and pais == "PRY":
+                cliente.update({"ruc": self.partner_id.vat}) #D206
+                cliente.update({"contribuyente": True}) #D201
+            else:
+                cliente.update({"contribuyente": False}) #D201
+                if pais == 'PRY':
+                    cliente.update(
+                        {"documentoTipo": self.partner_id.l10n_latam_identification_type_id.l10n_aipy_dnit_code}) #D208
+                    cliente.update({"documentoNumero": self.partner_id.vat}) #D210
+
+        if self.partner_id.l10n_latam_identification_type_id.l10n_aipy_dnit_code == 0:
+            if self.partner_id.is_company:
+                cliente.update({"tipoOperacion": 1}) #D202 B2B
+                cliente.update({"tipoContribuyente": 2}) #D205 Persona Juridica
+            else:
+                cliente.update({"tipoOperacion": 2}) #D202 B2C
+                cliente.update({"tipoContribuyente": 1}) #D205 Persona Fisica
+            if self.partner_id.country_id.code != 'PY':
+                cliente.update({"tipoOperacion": 4}) #D202 B2F
+        else:
+            if self.partner_id.country_id.code == 'PY':
+                cliente.update({"tipoOperacion": 2}) #D202 B2C
+                cliente.update({"tipoContribuyente": 1}) #D205 Persona Fisica
+            else:
+                cliente.update({"tipoOperacion": 2}) #D202 B2F
+                cliente.update({"tipoContribuyente": 2 if self.partner_id.is_company else 1}) #D205
+
+        razonSocial = self.partner_id.name
+        if razonSocial:
+            cliente.update({"razonSocial": razonSocial}) #D211
+        else:
+            cliente.update({"razonSocial": "Sin Nombre"}) #D211
+
+        # Direccion
+        cliente.update({"pais": pais}) #D203
+        if departamento and distrito and ciudad and pais == 'PRY':
+            cliente.update({"departamento": departamento}) #D219
+            cliente.update({"distrito": distrito}) #D221
+            cliente.update({"ciudad": ciudad}) #D223
+            cliente.update({"direccion": self.partner_id.street}) #D213
+            cliente.update({"numeroCasa": self.partner_id.l10n_aipy_house if self.partner_id.l10n_aipy_house else 0}) #D218
+        else:
+            cliente.update({"direccion": self.partner_id.street}) #D213
+            cliente.update({"numeroCasa": self.partner_id.l10n_aipy_house if self.partner_id.l10n_aipy_house else 0}) #D218
+        if self.partner_id.phone:
+            cliente.update({"telefono": self.partner_id.phone}) #D214
+        if self.partner_id.mobile:
+            cliente.update({"celular": self.partner_id.mobile}) #D215
+        if self.partner_id.email:
+            cliente.update({"email": self.partner_id.email}) #D215
+        return cliente
+
     #############################
 
 
@@ -158,20 +310,44 @@ class AccountMove(models.Model):
         # Initialize data
         self._initialize_data()
         # Set Params
-        self._setP("ruc", self.company_id.vat) #D102
+        self._setP("version", 150) #AA002
+        self._compute_timbrado()
+        self._setP("ruc", self.company_id.vat) #D101
         self._setP("tipoContribuyente", 1 if self.company_id.partner_id.company_type == 'person' else 2) #D103
+        self._compute_regimeType()
         self._setP("razonSocial", self.company_id.name) #D105
         if self.company_id.l10n_aipy_fantasy_name:
             self._setP("nombreFantasia", self.company_id.l10n_aipy_fantasy_name) #D106
         if self.company_id.l10n_aipy_testing_mode:
             self._setP("nombreFantasia", self.company_id.name)
             self._setP("razonSocial", "DE generado en ambiente de prueba - sin valor comercial ni fiscal") #D105
-        self._compute_regimeType()
-
-        self._setP("actividadesEconomicas", self._compute_economic_activity())
         self._setP("establecimientos", self._compute_estabecimientos())
+        self._compute_economic_activity()
 
         # Set Data
+        self._setD("fecha", self._compute_digital_date()) #A004
+        self._setD("codigoSeguridadAleatorio", self._generate_random_code()) #B004
+        self._setD("tipoEmision", 1 ) #B002
+        tipoDocumento = self.move_type
+        if tipoDocumento == 'out_invoice':
+            self._setD("tipoDocumento", 1) #C002
+        elif tipoDocumento == 'out_refund':
+            self._setD("tipoDocumento", 5) #C002
+        elif tipoDocumento == 'out_receipt':
+            self._setD("tipoDocumento", 7) #C002
+        elif tipoDocumento == 'in_refund':
+            self._setD("tipoDocumento", 6) #C002
+        else:
+            raise UserError(_("Document type is invalid (%s)" % tipoDocumento))
+        self._compute_sequence_number()
+        #
+        self._setD("moneda", self.currency_id.name) #D015
+        if self.currency_id.name != 'PYG':
+            self._setD("cambio", 1 / self.invoice_currency_rate) #D018
+            self._setD("condicionTipoCambio", 1) #D017
+        #
+        self._setD("cliente", self._compute_client())
+        #
         self._setD("items", self._compute_statement_lines())
         #
         _logger.info( "----- JSON PARAMS ----")
