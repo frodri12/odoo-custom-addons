868c4103a7b287ff7ecd31c5fd73c41a50f8003e
frodri12 <fndorod@gmail.com>
2025-05-08 19:56:36 -0300

    Create Fiscal Position

--- l10n_py_account/models/__init__.py
+++ l10n_py_account/models/__init__.py
@@ -4,6 +4,7 @@ from . import l10n_latam_identification_type
 from . import l10n_py_dnit_responsibility_type
 from . import uom_uom
 from . import l10n_latam_document_type
+from . import account_fiscal_position
 
 from . import res_company
 from . import res_partner
---
+++ l10n_py_account/models/account_fiscal_position.py
@@ -0,0 +1,20 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models, api, _
+
+class AccountFiscalPosition(models.Model):
+
+    _inherit = 'account.fiscal.position'
+
+    l10n_ar_afip_responsibility_type_ids = fields.Many2many(
+        'l10n_py.dnit.responsibility.type', 'l10n_py_dnit_reponsibility_type_fiscal_pos_rel',
+        string='DNIT Responsibility Types', help='List of DNIT responsibilities where this fiscal position '
+        'should be auto-detected')
+
+    def _get_fpos_ranking_functions(self, partner):
+        if self.env.company.country_id.code != "PY":
+            return super()._get_fpos_ranking_functions(partner)
+        return [
+            ('l10n_py_dnit_responsibility_type_id', lambda fpos: (
+                partner.l10n_py_dnit_responsibility_type_id in fpos.l10n_py_dnit_responsibility_type_ids
+            ))
+        ] + super()._get_fpos_ranking_functions(partner)
